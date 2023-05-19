46c0009ed3666eba1d9c23fb51fa7be316d9bf98
Jung Won <93530934+dhwiwwx@users.noreply.github.com>
2023-05-19 15:12:50 +0900

    Musinsa Rank API연결

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,17 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 import { useState, useEffect } from "react";
 import MainComponent from "./components/MainComponents";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 function App() {
+  const queryClient = new QueryClient();
   const [number, setNumber] = useState(0);
 
   return (
-    <ChakraProvider>
-      <MainComponent />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <MainComponent />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
--- my-app/src/components/MainComponents/index.js
+++ my-app/src/components/MainComponents/index.js
@@ -1,25 +1,56 @@
-import { Box, Center, Text, Flex, Heading, SimpleGrid } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
+import { Box, Center, SimpleGrid, Flex, Text } from "@chakra-ui/react";
 
+import { useQuery } from "@tanstack/react-query";
+import { getData } from "../../API/getData";
 function MainComponent() {
+  const { data, isLoading, error } = useQuery(["wear"], () => getData(), {
+    initialData: { data: [] },
+  });
+  console.log(data);
+
   return (
     <>
       <Center>
-        <Flex w={"40vh"} h={"100vh"} bg={"#eee"} flexDirection={"column"}>
+        <Flex w={"40vh"} h={"100vh"} flexDir={"column"}>
           <Text
             margin={"0 auto"}
             lineHeight={"10vh"}
-            fontSize={"2xl"}
-            fontWeight={"100"}
+            fontSize={"3xl"}
+            fontWeight="100"
           >
             무신사 랭킹
           </Text>
-          <SimpleGrid columns={1} spacing={10}>
-            <Box bg="tomato" height="80px"></Box>
-            <Box bg="tomato" height="80px"></Box>
-            <Box bg="tomato" height="80px"></Box>
-            <Box bg="tomato" height="80px"></Box>
-            <Box bg="tomato" height="80px"></Box>
+          <SimpleGrid bg={"#eee"} columns={1} spacingY={5} padding={5}>
+            {data.data.map((item, index) => {
+              return (
+                <Box
+                  display={"flex"}
+                  flexDirection={"column"}
+                  justifyContent={"space-around"}
+                  height="110px"
+                  boxShadow={"md"}
+                  bg={"#fff"}
+                  borderRadius={"lg"}
+                  fontWeight="400"
+                >
+                  <Text paddingLeft={4}>{item[3]}</Text>
+                  <Flex
+                    h={"80px"}
+                    alignItems={"end"}
+                    flexDir={"row"}
+                    justifyContent={"space-between"}
+                    lineHeight={"60px"}
+                    fontWeight="400"
+                  >
+                    <Text paddingLeft={4}>{item[4]}</Text>
+                    <Text w={"70px"} textAlign={"center"}>
+                      {item[7]}
+                    </Text>
+                  </Flex>
+                </Box>
+              );
+            })}
           </SimpleGrid>
         </Flex>
       </Center>
